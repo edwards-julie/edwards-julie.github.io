e5adda5de748907681322f1cd270b65408b849be
Julie Edwards <julieedwards@Julies-MacBook-Pro.local>
2024-04-13 15:40:13 -0700

    first blog post

--- blog/posts.js
+++ blog/posts.js
@@ -1,16 +1,16 @@
 // summary length ~ 36 words
 const posts = [
+    // {
+    //     title: "OUUC 2024 SUURF News #2",
+    //     date: "April ?th 2024",
+    //     summary: "Greetings fellow book lovers! Next post will be here",
+    //     href: "newsy-bits-2",
+    // },
     {
-        title: "OUUC 2024 Summer UU Reading Fun Event - Newsy Bits & Shopping Tips #1",
+        title: "OUUC 2024 SUURF News #1",
         date: "April 11th 2024",
         summary: "Greetings fellow book lovers! We now have dates for both of OUUC's community book sales - so please mark your calendars, bring on the book donations, and help spread the word! 2024 SUURF: OUUC's Summer UU Reading Fun",
         href: "newsy-bits-1",
-    },
-    {
-        title: "OUUC 2024 Summer UU Reading Fun Event - Newsy Bits & Shopping Tips #2",
-        date: "April ?th 2024",
-        summary: "Greetings fellow book lovers! Next post will be here",
-        href: "newsy-bits-2",
     },
 ]
 
